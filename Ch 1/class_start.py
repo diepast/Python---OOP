12dd2548ac253a13fb0998a8be3bf148ae58bed5
diego <dmpastuch@gmail.com>
2021-07-27 22:35:57 -0300

    Update class_start.py

--- Ch 1/class_start.py
+++ Ch 1/class_start.py
@@ -4,10 +4,14 @@
 
 class Book:
     # TODO: Properties defined at the class level are shared by all instances
+    BOOK_TYPES = ("HARDCOVER", "PAPERBACK", "EBOOK")
 
     # TODO: double-underscore properties are hidden from other classes
 
     # TODO: create a class method
+    @classmethod
+    def getbooktypes(cls):
+        return cls.BOOK_TYPES
 
     # TODO: create a static method
 
@@ -16,15 +20,20 @@ class Book:
     def setTitle(self, newtitle):
         self.title = newtitle
 
-    def __init__(self, title):
+    def __init__(self, title, booktype):
         self.title = title
+        if (not booktype in Book.BOOK_TYPES):
+            raise ValueError(f"{booktype} is not a valid book type")
+        else:
+            self.booktype = booktype
 
 
 # TODO: access the class attribute
+print("Book types ", Book.getbooktypes())
 
 
 # TODO: Create some book instances
-b1 = Book("Title 1")
-b2 = Book("Title 2")
+b1 = Book("Title 1", "HARDCOVER")
+b2 = Book("Title 2", "PAPERBACK")
 
 # TODO: Use the static method to access a singleton object
